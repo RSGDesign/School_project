8e354f1f9fe4a33f2bd5f1ecfa03df45a0df674f
RSGDesign <75441866+RSGDesign@users.noreply.github.com>
2021-01-31 23:45:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ echo ' <div id="reg-head-fail" class="headrg">Registration Failed! <br> Error: '
 }
 ?>
 
-
+test
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="js/bootstrap.bundle.min.js"></script>
